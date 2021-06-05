3f3ddd0a3f07fe8ea63b773178acfe880ed79f88
Aiden Grossman <agrossman154@yahoo.com>
2021-06-04 23:42:47 -0700

    Probably made some changes

--- backup/deploy-velero.sh
+++
@@ -1,9 +0,0 @@
-velero install \
-    --provider aws \
-    --plugins velero/velero-plugin-for-aws:v1.0.0 \
-    --bucket velero \
-    --secret-file ./credentials-velero \
-    --use-volume-snapshots=false \
-    --use-restic \
-    --default-volumes-to-restic \
-    --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=http://192.168.1.38:9000
--- backup/startminio.sh
+++
@@ -1,5 +0,0 @@
-docker run -d -p 9000:9000 \
-    -e "MINIO_ROOT_USER=kubernetesbackup" \
-    -e "MINIO_ROOT_PASSWORD=hGSKjH9u" \
-    -v /mnt/backupdrive/clusterbackup:/data \
-    minio/minio server /data 
