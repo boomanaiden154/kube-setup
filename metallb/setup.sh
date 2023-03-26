d3263bb8d06e2ab571f5087af50215166a54e10c
Aiden Grossman <agrossman154@yahoo.com>
2023-03-26 20:04:16 +0000

    Updated metallb, minor changes to vaultwarden

--- metallb/setup.sh
+++ metallb/setup.sh
@@ -1,4 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl apply -f kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f configMap.yaml
+#TODO: apply new configs
