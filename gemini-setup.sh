a76302f37c10eb7ebdf69f5c70d2b955c3e8d77f
Aiden Grossman <agrossman154@yahoo.com>
2025-11-30 22:05:11 +0000

    redo piraeus setup

--- gemini-setup.sh
+++
@@ -1,3 +0,0 @@
-kubectl create ns gemini
-helm repo add fairwinds-stable https://charts.fairwinds.com/stable
-helm install gemini fairwinds-stable/gemini --namespace gemini
--- piraeus/deploy.sh
+++ piraeus/deploy.sh
@@ -1,1 +1,2 @@
-helm upgrade piraeus-op ../piraeus-operator/charts/piraeus -f lvmConfig.yaml --namespace piraeus --create-namespace --set stork.enabled=false --set operator.satelliteSet.kernelModuleInjectionImage="quay.io/piraeusdatastore/drbd9-focal" --version 1.8.0
+helm upgrade --install --create-namespace --namespace piraeus-datastore piraeus-operator oci://ghcr.io/piraeusdatastore/piraeus-operator/piraeus --set installCRDs=true --wait --version 2.10.2
+helm upgrade --install --namespace piraeus-datastore linstor-cluster oci://ghcr.io/piraeusdatastore/helm-charts/linstor-cluster -f config.yaml --version 1.1.1
