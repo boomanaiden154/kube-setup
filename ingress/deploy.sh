0b4053b36dfcdb7dc1110717375006fcd3b9d351
Aiden Grossman <agrossman154@yahoo.com>
2025-12-01 05:09:33 +0000

    misc cleanup

--- ingress/deploy.sh
+++
@@ -1,3 +0,0 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm install nginx-ingress ingress-nginx/ingress-nginx -f nginxValues.yaml --namespace nginx-ingress --create-namespace --wait
--- ingress/installCertManager.sh
+++
@@ -1,1 +0,0 @@
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.12.0/cert-manager.yaml
