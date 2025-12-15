08157b963a47b7a13213216bb7bb8eb0d53d32f5
Aiden Grossman <agrossman154@yahoo.com>
2025-12-15 20:18:44 +0000

    finish jellyfin

--- jellyfin/deploy.sh
+++
@@ -1,6 +0,0 @@
-kubectl create namespace jellyfin
-kubectl apply -f jellyfin-storageclass.yaml -n jellyfin
-kubectl apply -f pvc.yaml -n jellyfin
-kubectl apply -f webdav-deployment.yaml -n jellyfin
-kubectl apply -f webdav-service.yaml -n jellyfin
-kubectl apply -f webdav-ingress.yaml -n jellyfin
